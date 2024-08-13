e04bc2d22bace2d68dc3887ba085c5aeeaafb2cf
mendelsshop <mendelsshop@gmail.com>
2024-08-13 16:33:54 -0400

    Moved most of the old tests to the correct place

--- src/expander.rs
+++ src/expander.rs
@@ -517,7 +517,10 @@ macro_rules! list {
 mod tests {
     use std::collections::BTreeSet;
 
-    use crate::ast::{bound_identifier, Ast, Scope, Symbol, Syntax};
+    use crate::{
+        ast::{bound_identifier, AdjustScope, Ast, Scope, Symbol, Syntax},
+        UniqueNumberManager,
+    };
 
     #[test]
     fn bound_identifier_same() {
@@ -525,21 +528,21 @@ mod tests {
         assert!(bound_identifier(
             Ast::Syntax(Syntax("a".into(), BTreeSet::from([Scope(0)]))),
             Ast::Syntax(Syntax("a".into(), BTreeSet::from([Scope(0)])))
-        ))
+        ));
     }
     #[test]
     fn bound_identifier_different_symbol() {
         assert!(!bound_identifier(
             Ast::Syntax(Syntax("a".into(), BTreeSet::from([Scope(0)]))),
             Ast::Syntax(Syntax("b".into(), BTreeSet::from([Scope(0)])))
-        ))
+        ));
     }
     #[test]
     fn bound_identifier_different_scope() {
         assert!(!bound_identifier(
             Ast::Syntax(Syntax("a".into(), BTreeSet::from([Scope(0)]))),
             Ast::Syntax(Syntax("a".into(), BTreeSet::from([Scope(1)])))
-        ))
+        ));
     }
 
     #[test]
@@ -585,6 +588,115 @@ mod tests {
                 Ast::Syntax(Syntax("b".into(), BTreeSet::new())),
                 Ast::Syntax(Syntax("c".into(), BTreeSet::new())),
             ]
+        );
+    }
+
+    #[test]
+    fn syntax_to_datum_with_identifier() {
+        assert_eq!(
+            Ast::Syntax(Syntax("a".into(), BTreeSet::new())).syntax_to_datum(),
+            Ast::Symbol(Symbol("a".into(), 0)),
+        );
+    }
+
+    #[test]
+    fn syntax_to_datum_with_number() {
+        assert_eq!(Ast::Number(1.0).syntax_to_datum(), Ast::Number(1.0));
+    }
+
+    #[test]
+    fn syntax_to_datum_with_list() {
+        assert_eq!(
+            list![
+                Ast::Syntax(Syntax("a".into(), BTreeSet::new())),
+                Ast::Syntax(Syntax("b".into(), BTreeSet::new())),
+                Ast::Syntax(Syntax("c".into(), BTreeSet::new())),
+            ]
+            .syntax_to_datum(),
+            list![
+                Ast::Symbol(Symbol("a".into(), 0)),
+                Ast::Symbol(Symbol("b".into(), 0)),
+                Ast::Symbol(Symbol("c".into(), 0)),
+            ]
+        );
+    }
+
+    #[test]
+    fn scope_equality_test() {
+        let mut scope_creator = UniqueNumberManager::new();
+        let sc1 = scope_creator.new_scope();
+        let sc2 = scope_creator.new_scope();
+        assert_ne!(sc1, sc2);
+        assert_eq!(sc2, sc2);
+    }
+    #[test]
+    fn add_scope_test_empty_scope() {
+        let mut scope_creator = UniqueNumberManager::new();
+        let sc1 = scope_creator.new_scope();
+        assert_eq!(
+            Ast::Syntax(Syntax("x".into(), BTreeSet::new())).add_scope(sc1),
+            Ast::Syntax(Syntax("x".into(), BTreeSet::from([sc1])))
+        );
+    }
+
+    #[test]
+    fn add_scope_test_empty_scope_list() {
+        let mut scope_creator = UniqueNumberManager::new();
+        let sc1 = scope_creator.new_scope();
+        assert_eq!(
+            list![
+                Ast::Symbol(Symbol("x".into(), 0)),
+                Ast::Symbol(Symbol("y".into(), 0)),
+            ]
+            .datum_to_syntax()
+            .add_scope(sc1),
+            list![
+                Ast::Syntax(Syntax("x".into(), BTreeSet::from([sc1]))),
+                Ast::Syntax(Syntax("y".into(), BTreeSet::from([sc1]))),
+            ]
+        );
+    }
+
+    #[test]
+    fn add_scope_test_non_empty_scope() {
+        let mut scope_creator = UniqueNumberManager::new();
+        let sc1 = scope_creator.new_scope();
+        let sc2 = scope_creator.new_scope();
+        assert_eq!(
+            Ast::Syntax(Syntax("x".into(), BTreeSet::from([sc1]))).add_scope(sc2),
+            Ast::Syntax(Syntax("x".into(), BTreeSet::from([sc1, sc2])))
+        );
+    }
+
+    #[test]
+    fn add_scope_test_add_duplicate() {
+        let mut scope_creator = UniqueNumberManager::new();
+        let sc1 = scope_creator.new_scope();
+        assert_eq!(
+            Ast::Syntax(Syntax("x".into(), BTreeSet::from([sc1]))).add_scope(sc1),
+            Ast::Syntax(Syntax("x".into(), BTreeSet::from([sc1,])))
+        );
+    }
+
+    #[test]
+    fn flip_scope_test_different() {
+        let mut scope_creator = UniqueNumberManager::new();
+        let sc1 = scope_creator.new_scope();
+        let sc2 = scope_creator.new_scope();
+        assert_eq!(
+            Ast::Syntax(Syntax("x".into(), BTreeSet::from([sc1]))).flip_scope(sc2),
+            Ast::Syntax(Syntax("x".into(), BTreeSet::from([sc1, sc2])))
+        );
+    }
+
+    #[test]
+    fn flip_scope_test_same() {
+        let mut scope_creator = UniqueNumberManager::new();
+        let sc1 = scope_creator.new_scope();
+        let sc2 = scope_creator.new_scope();
+        assert_eq!(
+            Ast::Syntax(Syntax("x".into(), BTreeSet::from([sc1, sc2]))).flip_scope(sc2),
+            Ast::Syntax(Syntax("x".into(), BTreeSet::from([sc1,])))
         );
     }
 }
