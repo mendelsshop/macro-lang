fc12de3418cdc4c4c6108d0553dd89c5ddc1195f
mendelsshop <mendelsshop@gmail.com>
2025-01-28 22:54:55 -0500

    fixed(ish) namespace repr although i with i did not have to use rc/refcell started to implement operations on namespace

--- src/expander/namespace.rs
+++ src/expander/namespace.rs
@@ -1,18 +1,27 @@
-use std::collections::HashMap;
+use std::{
+    cell::{Ref, RefCell},
+    collections::HashMap,
+    rc::Rc,
+};
 
 use crate::ast::{
     scope::{MultiScope, MutableMap, Scope, ShiftedMultiScope},
     Ast, Symbol,
 };
 
-use super::{binding::CompileTimeBinding, phase::Phase};
+use super::{
+    binding::{Binding, CompileTimeBinding},
+    phase::Phase,
+};
 
 pub type ResolvedModuleName = Symbol;
 #[derive(Clone, Debug)]
 pub struct Module {
-    pub self_name: Symbol,
+    pub self_name: ResolvedModuleName,
+    // immutable
     pub requires: HashMap<Phase, Vec<ResolvedModuleName>>,
-    pub provides: HashMap<Phase, Vec<ResolvedModuleName>>,
+    // immutable
+    pub provides: HashMap<Phase, HashMap<Symbol, Binding>>,
     pub min_phase_level: Phase,
     pub max_phase_level: Phase,
     pub instantiate: fn(NameSpace, Phase, Phase),
@@ -23,7 +32,7 @@ impl Module {
     pub fn new(
         self_name: Symbol,
         requires: HashMap<Phase, Vec<ResolvedModuleName>>,
-        provides: HashMap<Phase, Vec<ResolvedModuleName>>,
+        provides: HashMap<Phase, HashMap<Symbol, Binding>>,
         min_phase_level: Phase,
         max_phase_level: Phase,
         instantiate: fn(NameSpace, Phase, Phase),
@@ -38,14 +47,18 @@ impl Module {
         }
     }
 }
-// Currently just keeps top level bindings + transformers
 #[derive(Clone, Debug)]
 pub struct NameSpace {
     pub scope: Scope,
-    pub phases: HashMap<Phase, Definitions>,
-    pub module_declarations: HashMap<ResolvedModuleName, Module>,
-    pub submodule_declarations: HashMap<ResolvedModuleName, Module>,
-    pub module_instances: HashMap<(ResolvedModuleName, Phase), NameSpace>,
+    // TODO: maybe use ecs/global database structure as opposed to rc refcelling everything - could
+    // do same thing for scopes
+    // all the hashmaps look like the mutable
+    pub phases: MutableMap<Phase, Definitions>,
+    // all module declarations avaliable in any namespace?
+    // seems to be same object in all namespaces
+    pub module_declarations: MutableMap<ResolvedModuleName, Module>,
+    pub submodule_declarations: MutableMap<ResolvedModuleName, Module>,
+    pub module_instances: MutableMap<(ResolvedModuleName, Phase), NameSpace>,
 }
 
 #[derive(Clone, Debug)]
@@ -61,18 +74,49 @@ impl Default for NameSpace {
                 Phase(0),
                 MultiScope(MutableMap::default()),
             )),
-            phases: HashMap::new(),
-            module_declarations: HashMap::new(),
-            submodule_declarations: HashMap::new(),
-            module_instances: HashMap::new(),
+            phases: MutableMap::default(),
+            module_declarations: MutableMap::default(),
+            submodule_declarations: MutableMap::default(),
+            module_instances: MutableMap::default(),
         }
     }
 }
 
 impl NameSpace {
-    fn namespace_to_module(&self, name: &ResolvedModuleName) -> Option<&Module> {
-        self.module_declarations
-            .get(name)
-            .or_else(|| self.submodule_declarations.get(name))
+    fn make_module_namespace(&mut self, name: ResolvedModuleName, for_submodule: bool) -> Self {
+        let module_namespace = NameSpace {
+            submodule_declarations: if for_submodule {
+                Rc::clone(&self.submodule_declarations)
+            } else {
+                MutableMap::default()
+            },
+            module_declarations: Rc::clone(&self.module_declarations),
+            ..Default::default()
+        };
+        self.module_instances
+            .borrow_mut()
+            .insert((name, Phase(0)), module_namespace.clone());
+        module_namespace
+    }
+    fn namespace_to_module(&self, name: &ResolvedModuleName) -> Option<Ref<'_, Module>> {
+        {
+            Ref::filter_map(self.module_declarations.borrow(), |borrow| borrow.get(name))
+                .or_else(|_| {
+                    Ref::filter_map(self.submodule_declarations.borrow(), |borrow| {
+                        borrow.get(name)
+                    })
+                })
+                .ok()
+        }
+    }
+    /// as_submodule: defualts to false
+    fn declare_module(&self, name: ResolvedModuleName, m: Module, as_submodule: bool) {
+        if as_submodule {
+            self.submodule_declarations.clone()
+        } else {
+            self.module_declarations.clone()
+        }
+        .borrow_mut()
+        .insert(name, m);
     }
 }
