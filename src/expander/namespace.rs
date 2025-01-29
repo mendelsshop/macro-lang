38ceadde78c16474f0146707bbf1f4aa40546f26
mendelsshop <mendelsshop@gmail.com>
2025-01-29 11:40:19 -0500

    namespace stuff mostly done

--- src/expander/namespace.rs
+++ src/expander/namespace.rs
@@ -57,10 +57,12 @@ pub struct NameSpace {
     pub module_instances: MutableMap<(ResolvedModuleName, Phase), NameSpace>,
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Default)]
 pub struct Definitions {
     pub variables: HashMap<Symbol, Ast>,
     pub transformers: HashMap<Symbol, CompileTimeBinding>,
+    // although it says this should be mutable looking at how it is used seems to show its always
+    // true
     pub instantiated: bool,
 }
 impl Default for NameSpace {
@@ -108,5 +110,121 @@ impl NameSpace {
             self.module_declarations.clone()
         }
         .insert(name, m);
+    }
+
+    // min_phase: defualts to Phase(0)
+    fn namespace_module_instantiate(
+        &self,
+        name: &ResolvedModuleName,
+        phase_shift: Phase,
+        min_phase: Phase,
+    ) -> Result<(), String> {
+        // since we use create the error variant should never be none, if we had dependent types we
+        // wouldn't need the unwrap
+        let module_namespace = self
+            .namespace_to_module_namespace(name, phase_shift, true)
+            .map_err(Option::unwrap)?;
+
+        let module = self
+            .namespace_to_module(&name)
+            .ok_or(format!("no module found {name}"))?;
+        module
+            .requires
+            .iter()
+            .try_for_each(|(require_phase, modules)| {
+                modules.iter().try_for_each(|module| {
+                    self.namespace_module_instantiate(
+                        module,
+                        phase_shift + *require_phase,
+                        min_phase,
+                    )
+                })
+            })?;
+        (module.min_phase_level.0..module.max_phase_level.0 + 1)
+            .map(Phase)
+            .for_each(|phase_level| {
+                let phase = phase_level + phase_shift;
+                if phase >= min_phase {
+                    module_namespace.namespace_to_definitions(phase_level, |definitions| {
+                        if !definitions.instantiated {
+                            definitions.instantiated = true;
+                            (module.instantiate)(
+                                module_namespace.clone(),
+                                phase_shift,
+                                phase_level,
+                            );
+                        }
+                    });
+                }
+            });
+        Ok(())
+    }
+
+    fn namespace_module_visit(
+        &self,
+        name: &ResolvedModuleName,
+        phase: Phase,
+    ) -> Result<(), String> {
+        self.namespace_module_instantiate(name, phase, Phase(1))
+    }
+    // create: defaults to false
+    fn namespace_to_module_namespace(
+        &self,
+        name: &ResolvedModuleName,
+        phase: Phase,
+        create: bool,
+    ) -> Result<NameSpace, Option<String>> {
+        if let Some(v) = self
+            .module_instances
+            .get(&(name.clone(), phase))
+            .map(|c| c.clone())
+        {
+            Ok(v)
+        } else if !create {
+            Err(None)
+        } else if self.namespace_to_module(&name).is_none() {
+            Err(Some(format!("no module declared to instantiate {name}")))
+        } else {
+            let module_namespace = NameSpace {
+                module_declarations: self.module_declarations.clone(),
+                submodule_declarations: self.submodule_declarations.clone(),
+                module_instances: self.module_instances.clone(),
+                ..Default::default()
+            };
+            self.module_instances
+                .insert((name.clone(), phase), module_namespace.clone());
+            Ok(module_namespace)
+        }
+    }
+
+    fn namespace_to_definitions<T>(
+        &self,
+        phase_level: Phase,
+        k: impl FnOnce(&mut Definitions) -> T,
+    ) -> T {
+        self.phases
+            .entry(phase_level, |definitions| k(definitions.or_default()))
+    }
+
+    fn namespace_set_variable(&self, phase_level: Phase, name: Symbol, value: Ast) {
+        self.namespace_to_definitions(phase_level, |d| d.variables.insert(name, value));
+    }
+    fn namespace_set_transformer(
+        &self,
+        phase_level: Phase,
+        name: Symbol,
+        value: CompileTimeBinding,
+    ) {
+        self.namespace_to_definitions(phase_level, |d| d.transformers.insert(name, value));
+    }
+    fn namespace_get_variable(&self, phase_level: Phase, name: &Symbol) -> Option<Ast> {
+        self.namespace_to_definitions(phase_level, |d| d.variables.get(name).cloned())
+    }
+    fn namespace_get_transformer(
+        &self,
+        phase_level: Phase,
+        name: &Symbol,
+    ) -> Option<CompileTimeBinding> {
+        self.namespace_to_definitions(phase_level, |d| d.transformers.get(name).cloned())
     }
 }
